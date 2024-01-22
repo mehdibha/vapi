e6ff70445215ab962031c49b856dd860c6038b3a
Mehdi BHA <mehdibenhadjali@gmail.com>
2024-01-22 14:59:57 +0100

    fix: fix word break and white space wrap

--- src/modules/posts/components/create-post-card.tsx
+++ src/modules/posts/components/create-post-card.tsx
@@ -13,13 +13,15 @@ export const CreatePostCard = () => {
   return (
     <Card className="px-4 py-6 xs:px-6">
       <div className="flex items-center space-x-2 md:space-x-4">
-        <UserAvatar />
         {status === "authenticated" ? (
-          <CreatePostModal>
-            <div className="w-full cursor-pointer rounded-full border bg-muted/50 px-6 py-2 text-muted-foreground hover:bg-muted">
-              Vendez quelque chose ou posez une question.
-            </div>
-          </CreatePostModal>
+          <>
+            <UserAvatar />
+            <CreatePostModal>
+              <div className="w-full cursor-pointer rounded-full border bg-muted/50 px-6 py-2 text-muted-foreground hover:bg-muted">
+                Vendez quelque chose ou posez une question.
+              </div>
+            </CreatePostModal>
+          </>
         ) : (
           <LoginModal disabled={status === "loading"}>
             <div className="w-full cursor-pointer rounded-full border bg-muted/50 px-6 py-2 text-muted-foreground hover:bg-muted">
