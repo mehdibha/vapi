6971c3ba7b12fbbd6ed7880f0bac79873510dbac
Mehdi BHA <mehdibenhadjali@gmail.com>
2024-01-22 14:16:56 +0100

    feat: lazy loading for images

--- src/modules/posts/components/post-card.tsx
+++ src/modules/posts/components/post-card.tsx
@@ -62,6 +62,7 @@ export const PostCard = React.forwardRef(
                     <img
                       src={image}
                       alt="vape"
+                      loading="lazy"
                       className="h-[300px] w-full object-contain"
                     />
                   </CarouselItem>
